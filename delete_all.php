c2a2b09ebe6bcd77201c9fa0636b8ad6262e8aeb
SSG <ibot.robin@gmail.com>
2022-12-13 10:54:54 +0530

    some changes in the code 13/12/2022

--- delete_all.php
+++ delete_all.php
@@ -2,8 +2,8 @@
 // Load the database configuration file 
 include('./connection.php');
 // delete all records from the table
-$delete_query=$db->query("DELETE FROM students_details");
-if($delete_query){
-    echo "<script>alert('Successfully Deleted All The Records')</script>";  
+$delete_query = $db->query("DELETE FROM students_details");
+if ($delete_query) {
+    echo "<script>alert('Successfully Deleted All The Records')</script>";
 }
 echo "<script>window.location.href = 'index.php'</script>";
